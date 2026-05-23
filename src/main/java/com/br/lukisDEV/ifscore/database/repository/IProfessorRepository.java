ad0cf7ed85a3be52415d71e36c1704b060034f21
Joao Pedro <jjoaoxz20@gmail.com>
2026-05-23 15:19:03 -0300

    fix: emailException were not in use, add: CampusNotFoundException with custom error treatment

--- src/main/java/com/br/lukisDEV/ifscore/database/repository/IProfessorRepository.java
+++ src/main/java/com/br/lukisDEV/ifscore/database/repository/IProfessorRepository.java
@@ -1,6 +1,8 @@
 package com.br.lukisDEV.ifscore.database.repository;
 
 import com.br.lukisDEV.ifscore.database.model.ProfessorEntity;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,5 +11,7 @@ import java.util.UUID;
 
 public interface IProfessorRepository extends JpaRepository<ProfessorEntity, UUID> {
     Optional<ProfessorEntity> findByEmail(String email);
+
+    boolean existsByEmail(@NotBlank @Email String email);
 }
 
---
+++ src/main/java/com/br/lukisDEV/ifscore/exception/CampusNotFoundException.java
@@ -0,0 +1,7 @@
+package com.br.lukisDEV.ifscore.exception;
+
+public class CampusNotFoundException extends RuntimeException {
+    public CampusNotFoundException(String message) {
+        super(message);
+    }
+}
